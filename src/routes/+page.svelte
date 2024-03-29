762dc0a67662736c540858f97e4012d6ba09774a
asty8926 <31024172+asty8926@users.noreply.github.com>
2024-03-29 12:03:37 +0100

    added global config for sleep cycle value and printing it on home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+	import { config } from '$lib';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p>Sleep cycle value: {config.sleepCycle}</p>
